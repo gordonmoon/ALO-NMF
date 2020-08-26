e7824b924801345dfef21801c4002357daaebde2
Gordon E. Moon <euhyun.moon@gmail.com>
2020-08-26 14:12:40 -0600

    Update run_alo_nmf_cpu.sh

--- ALO-NMF_CPU/run_alo_nmf_cpu.sh
+++ ALO-NMF_CPU/run_alo_nmf_cpu.sh
@@ -1,10 +1,10 @@
-./nmf -est_nmf_cpu -K 64 -tile_size 8 -data 20newsgroups.txt -matrix_type 2 -V 26214 -D 11314 -niters 100
-# ./nmf -est_nmf_cpu -K 256 -tile_size 16 -data 20newsgroups.txt -matrix_type 2 -V 26214 -D 11314 -niters 100
-# ./nmf -est_nmf_cpu -K 1024 -tile_size 64 -data 20newsgroups.txt -matrix_type 2 -V 26214 -D 11314 -niters 100
+./nmf -est_nmf_cpu -K 64 -tile_size 8 -data ../20newsgroups.txt -matrix_type 2 -V 26214 -D 11314 -niters 100
+# ./nmf -est_nmf_cpu -K 256 -tile_size 16 -data ../20newsgroups.txt -matrix_type 2 -V 26214 -D 11314 -niters 100
+# ./nmf -est_nmf_cpu -K 1024 -tile_size 64 -data ../20newsgroups.txt -matrix_type 2 -V 26214 -D 11314 -niters 100
 
-# ./nmf -est_nmf_cpu -K 64 -tile_size 8 -data PIE.txt -matrix_type 1 -V 11554 -D 4096 -niters 100
-# ./nmf -est_nmf_cpu -K 256 -tile_size 16 -data PIE.txt -matrix_type 1 -V 11554 -D 4096 -niters 100
-# ./nmf -est_nmf_cpu -K 1024 -tile_size 64 -data PIE.txt -matrix_type 1 -V 11554 -D 4096 -niters 100
+# ./nmf -est_nmf_cpu -K 64 -tile_size 8 -data ../PIE.txt -matrix_type 1 -V 11554 -D 4096 -niters 100
+# ./nmf -est_nmf_cpu -K 256 -tile_size 16 -data ../PIE.txt -matrix_type 1 -V 11554 -D 4096 -niters 100
+# ./nmf -est_nmf_cpu -K 1024 -tile_size 64 -data ../PIE.txt -matrix_type 1 -V 11554 -D 4096 -niters 100
 
 # K = 64, T = 8
 # K = 128, T = 16
