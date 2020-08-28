6b4f39102e1a84a63f3bf9a47b7fd1f2833d5839
Gordon E. Moon <euhyun.moon@gmail.com>
2020-08-28 15:21:59 -0600

    Update run_alo_nmf_cpu.sh

--- ALO-NMF_CPU/run_alo_nmf_cpu.sh
+++ ALO-NMF_CPU/run_alo_nmf_cpu.sh
@@ -6,22 +6,9 @@
 # ./nmf -est_nmf_cpu -K 256 -tile_size 16 -data ../PIE.txt -matrix_type 1 -V 11554 -D 4096 -niters 100
 # ./nmf -est_nmf_cpu -K 1024 -tile_size 64 -data ../PIE.txt -matrix_type 1 -V 11554 -D 4096 -niters 100
 
+# The optimal tile size T for a given K
 # K = 64, T = 8
 # K = 128, T = 16
 # K = 256, T = 16
 # K = 512, T = 32
 # K = 1024, T = 64
-
-# matrix_type = 1 // Dense
-# matrix_type = 2 // Sparse
-
-# 20newsgroups.txt
-# V = 26214 D = 11314
-# TDT2.txt
-# V = 36771 D = 10212
-# p2p.txt
-# V = 36675(36682) D = 36682
-# MovieLens10M.txt
-# V = 71567 D = 10677
-# PIE.txt
-# V = 11554 D = 4096
